97a6391e523abe0a44b5696e8a706f4489c4d4a1
giuliFerreira <140911291+giulikaren@users.noreply.github.com>
2024-07-23 10:50:45 -0300

    clase solicitud

---
+++ POO Consulta Medica/Solicitud.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Consulta_Medica
+{
+    using System;
+
+    class Solicitud
+    {
+        private int NumeroInterno;
+        private DateTime FechaHoraSolicitud;
+        private bool AsistioConsulta;
+
+        Paciente _Paciente;
+        Consulta _Consulta;
+        public int _NumeroInterno
+        {
+            get { return NumeroInterno; }
+            set { NumeroInterno = value; }
+        }
+
+        public DateTime _FechaHoraSolicitud 
+        {
+            get { return FechaHoraSolicitud; }
+            set { FechaHoraSolicitud = value; }
+        }
+
+     
+        public Solicitud(int numeroIntern, DateTime fechaHoraSolicitud,bool asistencia)
+        {
+            NumeroInterno = numeroIntern;
+            FechaHoraSolicitud = fechaHoraSolicitud;
+            Asistencia = asistencia;
+        }
+        public bool _AsistioConsulta
+        {
+            get { return AsistioConsulta; }
+            set { AsistioConsulta = value; }
+        }
+
+        public void Asistencia(bool asistio)
+        {
+            AsistioConsulta = asistio;
+        }
+    }
